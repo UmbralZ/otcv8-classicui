7364eafcacdd381c8c951c569b9e38c32b32f53e
Tyson Strange <tyson@tls.id.au>
2022-06-20 22:47:42 +1000

    - Changed default app name to not conflict with default otcv8 appdata

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- CONFIG
-APP_NAME = "otclientv8"  -- important, change it, it's name for config dir and files in appdata
+APP_NAME = "OTCClassicUI"  -- important, change it, it's name for config dir and files in appdata
 APP_VERSION = 1341       -- client version for updater and login to identify outdated client
 DEFAULT_LAYOUT = "retro" -- on android it's forced to "mobile", check code bellow
 
@@ -29,7 +29,7 @@ Servers = {
 --USE_NEW_ENERGAME = true -- uses entergamev2 based on websockets instead of entergame
 ALLOW_CUSTOM_SERVERS = true -- if true it shows option ANOTHER on server list
 
-g_app.setName("OTCv8")
+g_app.setName("OTCClassicUI")
 -- CONFIG END
 
 -- print first terminal message
